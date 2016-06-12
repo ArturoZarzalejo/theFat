b9131173372cac5c75783397118d6846db2576b6
Arturozh <Arturoazh@gmail.com>
2016-06-12 14:48:58 +0200

    chore()
    Multiple changes

--- src/thefatfingerWebsite/modules/home/services/worksService.js
+++ src/thefatfingerWebsite/modules/home/services/worksService.js
@@ -21,15 +21,15 @@
 		scope.works = [{
       id: 1,
       name: 'ESPARTBCN',
-      url: 'http://inspiration.lumiart.cz/wp-content/uploads/2014/10/054_twitter-website-in-material-design.jpg'
+      url: 'assets/images/espartbcn.jpg'
     },{
       id: 2,
       name: '1 DIA',
-      url: 'https://camo.githubusercontent.com/499eda8efbd30966fd0326d062d5c1aa549a4dc9/687474703a2f2f7777772e6765746d646c2e696f2f6173736574732f676f6f676c652d73657276696365732e706e67'
+      url: 'assets/images/oneday.jpg'
     },{
       id: 2,
       name: 'GREENPEACE',
-      url: 'http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2015/07/1poXfoKp7KT-wOVy4kA92aw.png'
+      url: 'assets/images/greenpeace.jpg'
     }];
 
 
